8545800aea20bc4563f25f04e6c7a2b77e1215b4
Andrés Felipe Vargas <86252550+Lorddragonist@users.noreply.github.com>
2023-05-17 16:38:02 -0500

    Create lectorpdf.py

---
+++ lectorpdf.py
@@ -0,0 +1,33 @@
+import PyPDF2
+
+def extraer_comentarios(pdf_path):
+    comentarios = []
+    
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfFileReader(file)
+        num_paginas = reader.numPages
+        
+        for i in range(num_paginas):
+            pagina = reader.getPage(i)
+            comentarios_pagina = pagina.extractText()
+            
+            if '/Annots' in pagina:
+                anotaciones = pagina['/Annots'].getObject()
+                for anotacion in anotaciones:
+                    tipo = anotacion.getObject()['/Subtype']
+                    if tipo == '/Text':
+                        comentario = anotacion.getObject()['/Contents']
+                        comentarios.append(comentario)
+    
+    return comentarios
+
+# Ruta al archivo PDF
+pdf_path = 'ruta/al/archivo.pdf'
+
+# Llamada a la función para extraer comentarios
+comentarios_extraidos = extraer_comentarios(pdf_path)
+
+# Imprimir los comentarios extraídos
+for comentario in comentarios_extraidos:
+    print(comentario)
+
