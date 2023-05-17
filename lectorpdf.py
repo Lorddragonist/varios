ec93c990b2db792ecf93e80eefbe64fa3a03b198
Andrés Felipe Vargas <86252550+Lorddragonist@users.noreply.github.com>
2023-05-17 16:44:37 -0500

    Update lectorpdf.py

--- lectorpdf.py
+++ lectorpdf.py
@@ -33,3 +33,32 @@ for comentario in comentarios_extraidos:
     
 # PyPDF2.errors.DeprecationError: PdfFileReader is deprecated and was removed in PyPDF2 3.0.0. Use PdfReader instead.
 
+import PyPDF2
+
+def extraer_comentarios(pdf_path):
+    comentarios = []
+    
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        num_paginas = len(reader.pages)
+        
+        for i in range(num_paginas):
+            pagina = reader.pages[i]
+            comentarios_pagina = pagina.extract_text()
+            
+            for annot in pagina.annots:
+                if annot.subtype[1:-1] == 'Text':
+                    comentario = annot.contents
+                    comentarios.append(comentario)
+    
+    return comentarios
+
+# Ruta al archivo PDF
+pdf_path = 'ruta/al/archivo.pdf'
+
+# Llamada a la función para extraer comentarios
+comentarios_extraidos = extraer_comentarios(pdf_path)
+
+# Imprimir los comentarios extraídos
+for comentario in comentarios_extraidos:
+    print(comentario)
